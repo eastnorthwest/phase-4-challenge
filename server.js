615ca312359924f570cd644f516bafb76a449913
Steve McDonald <steve@amwlabs.com>
2017-07-24 15:03:18 -0700

    Add home page. Add routes for signup and signup.

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const database = require('./database')
+const app = express()
+
+require('ejs')
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false }))
+
+const users = require('./routes/users')
+
+app.get('/', (request, response) => {
+    database.getAlbums((error, albums) => {
+        if (error) {
+            response.status(500).render('error', { error: error })
+        } else {
+            response.render('index', { albums: albums })
+        }
+    })
+})
+
+app.get('/albums/:albumID', (request, response) => {
+    const albumID = request.params.albumID
+
+    database.getAlbumsByID(albumID, (error, albums) => {
+        if (error) {
+            response.status(500).render('error', { error: error })
+        } else {
+            const album = albums[0]
+            response.render('albums/album', { album: album })
+        }
+    })
+})
+
+app.use('/users/', users)
+
+app.use((request, response) => {
+    response.status(404).render('not_found')
+})
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log(`Listening on http://localhost:${port}...`)
+})
